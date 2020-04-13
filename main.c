83ae74d4256e649f7183f525ac3f6ed727214882
mpulido19 <60494640+mpulido19@users.noreply.github.com>
2020-04-12 17:05:51 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "slist.h"
+#include "dlist.h"
+
+//-------------------------------------------------
+int main(int argc, const char* argv[]) {
+  slist_test();
+  dlist_test();
+
+  return 0;
+}
